0da4d9c4559832d32f697ce09a925ef91d0cb9d5
DEKVIW <hxw1157816198@163.com>
2022-12-24 12:23:32 +0800

    Site updated: 2022-12-24 12:23:28

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "012",
+    "background": "5",
+    "tmpColor": "FFD700",
+    "tmpSize": "18",
+    "cityColor": "FFD700",
+    "citySize": "18",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "false",
+    "vertical": "top",
+    "horizontal": "left",
+    "key": "b003bb51706c4a69994939920191ca30"
+  }
+}
